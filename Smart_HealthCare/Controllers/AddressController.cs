76401e6fc1e0541447f9a344ac1d6742dd8022ea
Hridoy Roy <roy.hridoy2014@gmail.com>
2019-09-15 17:13:51 +0600

    Complete Division add, update and delete

--- Smart_HealthCare/Controllers/AddressController.cs
+++ Smart_HealthCare/Controllers/AddressController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Smart_HealthCare.Models;
@@ -9,12 +11,12 @@ namespace Smart_HealthCare.Controllers
 {
     public class AddressController : Controller
     {
-    
+
         Smart_HealthCareEntities1 db = new Smart_HealthCareEntities1();
         // GET: Address
         public ActionResult DivisionInformation()
         {
-            return View(db.Divisions.ToList());
+            return View(db.Divisions.OrderBy(Division => Division.DivisionName).ToList());
         }
 
         [HttpGet]
@@ -23,6 +25,72 @@ namespace Smart_HealthCare.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult DivisionCreate(Division division)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Divisions.Add(division);
+                db.SaveChanges();
+                return RedirectToAction("DivisionInformation", "Address");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DivisionUpdate(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Division division = db.Divisions.Find(id);
+
+            if (division == null)
+            {
+                return HttpNotFound();
+            }
+            return View(division);
+
+        }
+
+        [HttpPost]
+        public ActionResult DivisionUpdate([Bind (Include = "DivisionId,DivisionName")]Division division)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(division).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("DivisionInformation");
+            }
+            return View(division);
+        }
+
+        [HttpGet]
+        public ActionResult DivisionDelete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Division division = db.Divisions.Find(id);
+            if (division == null)
+            {
+                return HttpNotFound();
+            }
+            return View(division);
+        }   
         
+        [HttpPost]
+        public ActionResult DivisionDelete(int id)
+        {
+            Division division = db.Divisions.Find(id);
+            db.Divisions.Remove(division);
+            db.SaveChanges();
+            return RedirectToAction("DivisionInformation","Address");
+        }
     }
 }
